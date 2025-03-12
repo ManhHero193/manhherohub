15be42ddd72c09b512a1087962a803d8c401e421
ManhHero193 <158557275+ManhHero193@users.noreply.github.com>
2025-03-12 17:41:49 +0700

    Create curloz.lua

---
+++ curloz.lua
@@ -0,0 +1,5 @@
+{
+    Elements = {"Earth", "Fire", "Water", "Wind", "Lightning", "Ice"},
+    Clans = {"Haruno", "Bug", "Bone", "Namikaze", "Lee", "Hyuga", "Uchiha Madara", "Uchiha Sasuke", "Uchiha Obito", "Uchiha Itachi", "Uchiha Shisui", "Rinnegan", "Rikudou"},
+    Family = {"Healer", "Soul", "Shadow", "Bug", "Dog", "Expansion", "Bone", "Uzumaki", "Fighter", "Yellow Thunder", "Monkey", "Red Eyes", "White Eyes", "Wood", "Purple Eyes", "Rikudou"}
+}
