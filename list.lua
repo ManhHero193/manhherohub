61f0f284d12cafb0cc1025188472d19013cc8d02
ManhHero193 <158557275+ManhHero193@users.noreply.github.com>
2025-03-30 13:55:40 +0700

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,12 @@
+return {
+    Flow = {
+        "Genius", "Monster", "Wild Card", "Puzzle", "Lightning", "Demon Wings",
+        "Gale Burst", "King's Instinct", "Snake", "Prodigy", "Trap", "Chameleon",
+        "Dribbler", "Ice", "Crow", "Soul Harvester", "Awakened Genius", "Emperor"
+    },
+    Style = {
+        "Isagi", "Chigiri", "Bachira", "King", "Nagi", "Shidou", "Gagamaru",
+        "Rin", "Aiku", "Reo", "Yukimiya", "Hiori", "Karasu", "Otoya",
+        "Kunigami", "Sae", "Don Lorenzo", "Kurona", "NEL Isagi", "Kaiser"
+    }
+}
