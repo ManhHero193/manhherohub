bbaf3a214dab023901bff9091daa15c3cfa32680
ManhHero193 <158557275+ManhHero193@users.noreply.github.com>
2025-03-31 15:34:08 +0700

    Update curloz.lua

--- curloz.lua
+++ curloz.lua
@@ -1,5 +1,5 @@
 return {
-    Elements = {"Earth", "Fire", "Water", "Wind", "Lightning", "Ice"},
-    Clans = {"Haruno", "Bug", "Bone", "Namikaze", "Lee", "Hyuga", "Uchiha Madara", "Uchiha Sasuke", "Uchiha Obito", "Uchiha Itachi", "Uchiha Shisui", "Rinnegan", "Rikudou"},
+    Elements = {"Earth", "Fire", "Water", "Wind", "Lightning", "Ice", "Wood"},
+    Clans = {"Haruno", "Bug", "Bone", "Namikaze", "Lee", "Hyuga", "Uchiha Madara", "Uchiha Sasuke", "Uchiha Obito", "Uchiha Itachi", "Uchiha Shisui", "Thousand Hands", "Rinnegan", "Rikudou"},
     Family = {"Healer", "Soul", "Shadow", "Bug", "Dog", "Expansion", "Bone", "Uzumaki", "Fighter", "Yellow Thunder", "Monkey", "Red Eyes", "White Eyes", "Wood", "Purple Eyes", "Rikudou"}
 }
