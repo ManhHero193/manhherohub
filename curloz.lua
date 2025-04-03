40bdbaf4a65d2b1838cbaf1f4a9ba0174a1f570d
ManhHero193 <158557275+ManhHero193@users.noreply.github.com>
2025-04-03 17:37:44 +0700

    Update curloz.lua

--- curloz.lua
+++ curloz.lua
@@ -2,14 +2,4 @@ return {
     Elements = {"Earth", "Fire", "Water", "Wind", "Lightning", "Ice", "Wood"},
     Clans = {"Haruno", "Bug", "Bone", "Namikaze", "Lee", "Hyuga", "Uchiha Madara", "Uchiha Sasuke", "Uchiha Obito", "Uchiha Itachi", "Uchiha Shisui", "Thousand Hands", "Rinnegan", "Rikudou"},
     Family = {"Healer", "Soul", "Shadow", "Bug", "Dog", "Expansion", "Bone", "Uzumaki", "Fighter", "Yellow Thunder", "Monkey", "Red Eyes", "White Eyes", "Wood", "Purple Eyes", "Rikudou"}
-Flow = {
-        "Genius", "Monster", "Wild Card", "Puzzle", "Lightning", "Demon Wings",
-        "Gale Burst", "King's Instinct", "Snake", "Prodigy", "Trap", "Chameleon",
-        "Dribbler", "Ice", "Crow", "Soul Harvester", "Awakened Genius", "Emperor"
-    },
-    Style = {
-        "Isagi", "Chigiri", "Bachira", "King", "Nagi", "Shidou", "Gagamaru",
-        "Rin", "Aiku", "Reo", "Yukimiya", "Hiori", "Karasu", "Otoya",
-        "Kunigami", "Sae", "Don Lorenzo", "Kurona", "NEL Isagi", "Kaiser"
-    }
 }
