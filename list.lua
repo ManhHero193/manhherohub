f279dbe27e1658b2b2a268381c0fe1b219aab560
ManhHero193 <158557275+ManhHero193@users.noreply.github.com>
2025-04-03 17:38:56 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,12 +1,4 @@
 return {
-    Flow = {
-        "Genius", "Monster", "Wild Card", "Puzzle", "Lightning", "Demon Wings",
-        "Gale Burst", "King's Instinct", "Snake", "Prodigy", "Trap", "Chameleon",
-        "Dribbler", "Ice", "Crow", "Soul Harvester", "Awakened Genius", "Emperor"
-    },
-    Style = {
-        "Isagi", "Chigiri", "Bachira", "King", "Nagi", "Shidou", "Gagamaru",
-        "Rin", "Aiku", "Reo", "Yukimiya", "Hiori", "Karasu", "Otoya",
-        "Kunigami", "Sae", "Don Lorenzo", "Kurona", "NEL Isagi", "Kaiser"
-    }
+    Flow = {"Genius", "Monster", "Wild Card", "Puzzle", "Lightning", "Demon Wings","Gale Burst", "King's Instinct", "Snake", "Prodigy", "Trap", "Chameleon","Dribbler", "Ice", "Crow", "Soul Harvester", "Awakened Genius", "Emperor"},
+    Style = {"Isagi", "Chigiri", "Bachira", "King", "Nagi", "Shidou", "Gagamaru","Rin", "Aiku", "Reo", "Yukimiya", "Hiori", "Karasu", "Otoya","Kunigami", "Sae", "Don Lorenzo", "Kurona", "NEL Isagi", "Kaiser"}
 }
